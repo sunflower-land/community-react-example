465ca93ec4d1af87af70b771b846e0921a4b6f75
Adam Hannigan <adamhannigan81@gmail.com>
2023-08-21 17:40:30 +1000

    Add all files

--- src/MotherUnicorn.tsx
+++
@@ -1,9 +0,0 @@
-// import React from "react";
-
-// export const MotherUnicorn: React.FC = () => {
-//   return (
-//     <div className="p-2 bg-red-500">
-//       <p>Neighhhh!</p>
-//     </div>
-//   );
-// };
---
+++ src/ReactApp.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const AppContainer: React.FC = () => {
+  return (
+    <div className="absolute inset-0 flex items-center justify-center">
+      <p id="test-id" clas>
+        Hello world
+      </p>
+    </div>
+  );
+};
